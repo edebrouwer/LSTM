ed090c6e9c35b24733f6b7de73f3c4f90089b8de
Edward De Brouwer <edwarddebrouwer@Edwards-Machine.local>
2018-05-22 15:20:25 +0200

    detach'

--- LSTM_test.py
+++ LSTM_test.py
@@ -39,7 +39,7 @@ def testAUC_model(mod):
         data_ref=Variable(sample_batched[0][:,:,1:],requires_grad=False).cuda()
         out = mod.fwd_test(data_in)
         true_labs=sample_batched[1].numpy()
-        inferred_labs=out[1].numpy()
+        inferred_labs=out[1].detach().numpy()
 
     #Compute AUC.
     from sklearn.metrics import roc_auc_score
